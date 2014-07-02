4c7f4cfe1021fa0fb4c7dda701931fbf6a87dbac
larod <LAROD@LAROD-MacbookPro.local>
2014-07-01 21:47:13 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -5,6 +5,8 @@
 -(void)play
 {
     CCLOG(@"play button pressed");
+    CCScene *gameplayScene = [CCBReader loadAsScene:@"Gameplay"];
+    [[CCDirector sharedDirector] replaceScene:gameplayScene];
 }
 
 @end
