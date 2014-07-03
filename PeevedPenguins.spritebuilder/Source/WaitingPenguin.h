fc7959be288212b8ae85c1e5358a34331a371717
larod <LAROD@LAROD-MacbookPro.local>
2014-07-03 13:51:54 -0400

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -1,11 +1,3 @@
-//
-//  WaitingPenguin.h
-//  PeevedPenguins
-//
-//  Created by LAROD on 7/3/14.
-//  Copyright (c) 2014 Apportable. All rights reserved.
-//
-
 #import "CCSprite.h"
 
 @interface WaitingPenguin : CCSprite
